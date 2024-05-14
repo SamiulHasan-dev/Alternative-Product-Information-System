f0071be04fd6f4b98a5b80662d0001fe70273068
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-15 04:05:03 +0600

    modify search option

--- src/pages/MyQueries.jsx
+++ src/pages/MyQueries.jsx
@@ -66,7 +66,7 @@ const MyQueries = () => {
             </div>
             <div className="bg-base-100 pb-14 max-w-5xl mx-auto mt-6">
                 {
-                    queries?.length ? <h2 className=" p-5 rounded-lg font-bold text-2xl text-center w-full mb-10">Queries Added From My Side</h2> 
+                    queries?.length ? <h2 className=" p-5 rounded-lg font-bold text-2xl text-center w-full mb-10">Queries Added From Me</h2> 
                     :
                     <h2 className=" p-5 rounded-lg font-bold text-2xl text-center w-full my-7 md:my-16">Please Add Your Queries</h2>
                 }
